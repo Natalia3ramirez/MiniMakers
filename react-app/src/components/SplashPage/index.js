2cbec43cd56130b8402424477f79ee94865e6abd
Natalia Ramirez <email@example.com>
2023-08-20 20:14:24 -0700

    final

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -18,59 +18,61 @@ function SplashPage() {
       </div>
       <div className="splash-images">
         <div className="splash-images-container">
-        <ResponsiveMasonry
-          columnsCountBreakPoints={{ 350: 1, 750: 2, 1020: 7 }}
-        >
-          <Masonry className='mason' gutter="10px">
-          <img
-                src='https://i.pinimg.com/564x/bd/dc/f3/bddcf31fdb1edb050ade7801397a52fe.jpg'
-                alt='Photo 1'
-                className="splash-image"
-                style={{ height: '500px' }}
-              />
-            <img
-                src='https://i.pinimg.com/564x/22/d1/a4/22d1a4b95758e4ccd9389c5076a86496.jpg'
-                alt='Photo 2'
-                className="splash-image"
-                style={{ height: '400px'}}
-              />
-              <img
-                src='https://i.pinimg.com/736x/07/01/5d/07015d702eda67080700b0d57c1b644a.jpg'
-                alt='Photo 3'
-                className="splash-image"
-                style={{ height: '300px'}}
-              />
-              <img
-                src='https://i.pinimg.com/236x/d3/5d/85/d35d850035a7e084480645d9c4b88400.jpg'
-                alt='Photo 4'
-                className="splash-image"
-                style={{ height: '200px'}}
-              />
-              <img
-                src='https://i.pinimg.com/236x/4c/ff/e4/4cffe4c1e7feda4174c14b21a7cc0ea9.jpg'
-                alt='Photo 5'
-                className="splash-image"
-                style={{ height: '300px'}}
-              />
-              <img
-                src='https://i.pinimg.com/236x/50/d4/8a/50d48ac10e4bca3671f8f887439f29dd.jpg'
-                alt='Photo 6'
-                className="splash-image"
-                style={{ height: '400px'}}
-              />
-              <img
-                src='https://i.pinimg.com/474x/e0/3b/83/e03b837dfc16bf31182034311afc14f2.jpg'
-                alt='Photo 6'
-                className="splash-image"
-                style={{ height: '500px'}}
-              />
+          <div className="splash-grid">
+            <ResponsiveMasonry
+              columnsCountBreakPoints={{ 350: 1, 750: 2, 1020: 7 }}
+            >
+              <Masonry className='mason' gutter="10px">
+                <img
+                  src='https://i.pinimg.com/564x/bd/dc/f3/bddcf31fdb1edb050ade7801397a52fe.jpg'
+                  alt='Photo 1'
+                  className="splash-image"
+                  style={{ height: '500px' }}
+                />
+                <img
+                  src='https://i.pinimg.com/564x/22/d1/a4/22d1a4b95758e4ccd9389c5076a86496.jpg'
+                  alt='Photo 2'
+                  className="splash-image"
+                  style={{ height: '400px' }}
+                />
+                <img
+                  src='https://i.pinimg.com/736x/07/01/5d/07015d702eda67080700b0d57c1b644a.jpg'
+                  alt='Photo 3'
+                  className="splash-image"
+                  style={{ height: '300px' }}
+                />
+                <img
+                  src='https://i.pinimg.com/236x/d3/5d/85/d35d850035a7e084480645d9c4b88400.jpg'
+                  alt='Photo 4'
+                  className="splash-image"
+                  style={{ height: '200px' }}
+                />
+                <img
+                  src='https://i.pinimg.com/236x/4c/ff/e4/4cffe4c1e7feda4174c14b21a7cc0ea9.jpg'
+                  alt='Photo 5'
+                  className="splash-image"
+                  style={{ height: '300px' }}
+                />
+                <img
+                  src='https://i.pinimg.com/236x/50/d4/8a/50d48ac10e4bca3671f8f887439f29dd.jpg'
+                  alt='Photo 6'
+                  className="splash-image"
+                  style={{ height: '400px' }}
+                />
+                <img
+                  src='https://i.pinimg.com/474x/e0/3b/83/e03b837dfc16bf31182034311afc14f2.jpg'
+                  alt='Photo 6'
+                  className="splash-image"
+                  style={{ height: '500px' }}
+                />
 
 
-          </Masonry>
-        </ResponsiveMasonry>
+              </Masonry>
+            </ResponsiveMasonry>
+          </div>
         </div>
+        <div className="fade-effect"></div>
       </div>
-      <div className="fade-effect"></div>
     </div>
   );
 }
