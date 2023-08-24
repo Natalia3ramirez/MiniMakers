18d600bf7f241d370af2bd8cf0370ec28e49454f
Natalia Ramirez <email@example.com>
2023-08-23 21:26:14 -0700

    delete comments route

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -1,32 +1,26 @@
 
-
-# from flask import Blueprint, jsonify, request
-# from flask_login import current_user, login_required
-# from .auth_routes import validation_errors_to_error_messages
-# from app.models import Comment, db
-# from app.forms import CommentForm
-
-
-# comment_routes = Blueprint('comments', __name__)
+from flask import Blueprint, jsonify, request
+from flask_login import current_user, login_required
+from .auth_routes import validation_errors_to_error_messages
+from app.models import Comment, db
+from app.forms import CommentForm
 
 
-# Create a new Comment
-# @comment_routes.route('/<int:pinId>', methods=['POST'])
-# @login_required
-# def createComment(pinId):
+comment_routes = Blueprint('comments', __name__)
 
-#     form = CommentForm()
-#     form['csrf_token'].data = request.cookies['csrf_token']
-#     if form.validate_on_submit():
+# delete a comment
+@comment_routes.route('/<int:commentId>', methods=['DELETE'])
+@login_required
+def deleteComment(commentId):
+  current_user_id = current_user.to_dict()['id']
+  current_comment = Comment.query.get(commentId)
 
-#         new_comment = Comment(
-#             user_id = current_user.id,
-#             message = form.data['message'],
-#             pin_id = pinId,
-#         )
+  if not current_comment:
+    return {'errors': "comment not found"}, 400
+  if (current_user_id != current_comment.user_id):
+    return {'errors': "can only delete your own comment"}, 401
 
-#         db.session.add(new_comment)
-#         db.session.commit()
+  db.session.delete(current_comment)
+  db.session.commit()
 
-#         return new_comment.to_dict()
-#     return {"errors": validation_errors_to_error_messages(form.errors)}
+  return {"message":f"Successfully deleted Comment {commentId}"}
