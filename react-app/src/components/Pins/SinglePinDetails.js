8ea14ecd6a1a54f2fed56861d7c17f7de440ed46
Natalia Ramirez <natalia3ramirez@gmail.com>
2023-08-31 10:17:15 -0700

    fixed add to board bug

--- react-app/src/components/Pins/SinglePinDetails.js
+++ react-app/src/components/Pins/SinglePinDetails.js
@@ -50,16 +50,16 @@ export default function SinglePin() {
           <div className='details-save-pin'>
             <div id='delete-edit-pin-modal'>
               {user && pinUser && (
-              <OpenModalButton
-                buttonText="···"
-                modalComponent={<DeleteEditPin pin={pin} pinId={pin.id} />}
-              />
+                <OpenModalButton
+                  buttonText="···"
+                  modalComponent={<DeleteEditPin pin={pin} pinId={pin.id} />}
+                />
               )}
             </div>
             <div className='save-to-board'>
               <OpenModalButton
                 buttonText="Save to a Board"
-                modalComponent={<AddPinToBoard pin={pin} />}
+                modalComponent={<AddPinToBoard pin={pin} pin_id={pin.id} />}
               />
 
             </div>
@@ -67,62 +67,62 @@ export default function SinglePin() {
             </div>
           </div>
           <div className='scroll-bar-pin'>
-          <div className='pin-title-description'>
-            <h2>{pin.title}</h2>
-            <p>{pin.description}</p>
-          </div>
-
-          <div>
-            <div >
-              <div>
-                <div className='profile-user-image'>
-                  <img src={pin.user.image} alt={pin.user.firstName} />
-                  <p>@{pin.user.firstName}</p>
-                </div>
-              </div>
+            <div className='pin-title-description'>
+              <h2>{pin.title}</h2>
+              <p>{pin.description}</p>
             </div>
-          </div>
-          <div className='pin-comments-container'>
-            {comments.length ? (
-              <h3 className="comment-title">{pin.commentsLen} Comments</h3>
-            ) : (
-              <div className='comments'>
+
+            <div>
+              <div >
                 <div>
-                  <h4>Comments</h4>
-                  <p>No comments yet! Add one to start the conversation.</p>
-                </div>
-
-                <h3 className="comment-title">What do you think?</h3>
-              </div>
-            )
-            }
-
-            <div className='display-comments-container'>
-              {comments.map((comment) => (
-                <div key={comment.id}>
-                  <div className='user-comment-container'>
-                  <img className="pin-owner-icon" style={{ width: '25px', height: '25px' }} src={comment.user.image} alt={comment.user.firstName} />
-                  <h3>{comment.user.firstName}</h3>
-                  <p>{comment.message}</p>
+                  <div className='profile-user-image'>
+                    <img src={pin.user.image} alt={pin.user.firstName} />
+                    <p>@{pin.user.firstName}</p>
                   </div>
-                  <p>{new Date(comment.created_at).toLocaleDateString('en-US', { weekday: 'short', month: 'short', day: 'numeric' })}</p>
-                  {user.id === comment.user_id ? (
-                    <div className='delete-edit-comment-modal'>
-                      <OpenModalButton
-                        buttonText="···"
-                        modalComponent={<DeleteEditComment commentId={comment.id} pinId={pin.id} comment={comment} />}
-                      />
-                    </div>
-                  ) : ('')}
                 </div>
-              ))}
+              </div>
             </div>
-          </div>
+            <div className='pin-comments-container'>
+              {comments.length ? (
+                <h3 className="comment-title">{pin.commentsLen} Comments</h3>
+              ) : (
+                <div className='comments'>
+                  <div>
+                    <h4>Comments</h4>
+                    <p>No comments yet! Add one to start the conversation.</p>
+                  </div>
+
+                  <h3 className="comment-title">What do you think?</h3>
+                </div>
+              )
+              }
+
+              <div className='display-comments-container'>
+                {comments.map((comment) => (
+                  <div key={comment.id}>
+                    <div className='user-comment-container'>
+                      <img className="pin-owner-icon" style={{ width: '25px', height: '25px' }} src={comment.user.image} alt={comment.user.firstName} />
+                      <h3>{comment.user.firstName}</h3>
+                      <p>{comment.message}</p>
+                    </div>
+                    <p>{new Date(comment.created_at).toLocaleDateString('en-US', { weekday: 'short', month: 'short', day: 'numeric' })}</p>
+                    {user.id === comment.user_id ? (
+                      <div className='delete-edit-comment-modal'>
+                        <OpenModalButton
+                          buttonText="···"
+                          modalComponent={<DeleteEditComment commentId={comment.id} pinId={pin.id} comment={comment} />}
+                        />
+                      </div>
+                    ) : ('')}
+                  </div>
+                ))}
+              </div>
+            </div>
 
           </div>
-            <div className='submit-comment'>
-              <CreateComment pin={pin} />
-            </div>
+          <div className='submit-comment'>
+            <CreateComment pin={pin} />
+          </div>
         </div>
 
       </div>
